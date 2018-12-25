e7b382f2013a605abdcfdeaa6190f7e63232dd9d
shadow625 <wangpc625@gmail.com>
2018-12-26 00:41:46 +0800

    Description:	format the output ,and restrict the acceptable user rule. Type:		feature

--- chatTest.py
+++ chatTest.py
@@ -2,14 +2,18 @@ import itchat
 import requests
 import urllib
 import json
+import pprint
 
 url='http://openapi.tuling123.com/openapi/api/v2'
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
-	value={"reqType":0,"perception":{"inputText":{"text":msg.text}},"userInfo":{"apiKey":"501dba59c7f54555b0d9b66f4acabd94","userId":"235827"}}
-	req=requests.post(url,data=json.dumps(value))
+	if msg['User']['NickName'] != "五歲兒":
+		return
+	value={"reqType":0, "perception":{"inputText":{"text":msg.text}}, "userInfo":{"apiKey":"501dba59c7f54555b0d9b66f4acabd94","userId":"235827"}}
+	req=requests.post(url, data=json.dumps(value))
 	a=eval(req.text)
-	print(a)
-	return a.get("results")[0].get('values').get('text')
-itchat.auto_login()
+	reply = a.get("results")[0].get('values').get('text')
+	print("{\n\'msg\':\t\t%s\n\'reply\':\t%s\n}\n" % (msg.text, reply))
+	return reply
+itchat.auto_login(enableCmdQR=2, hotReload=True)
 itchat.run()
