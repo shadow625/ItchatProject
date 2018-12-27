e00e15df6f6b8199dd79283707e4f99be7dc9789
shadow625 <wangpc625@gmail.com>
2018-12-28 01:02:41 +0800

    Description:	add a switch for chatter Type:		feature

--- chatTest.py
+++ chatTest.py
@@ -1,19 +1,43 @@
 import itchat
 import requests
-import urllib
 import json
-import pprint
 
-url='http://openapi.tuling123.com/openapi/api/v2'
+url = 'http://openapi.tuling123.com/openapi/api/v2'
+mIsItchatEnabled = False
+
+
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
-	if msg['User']['NickName'] != "五歲兒":
-		return
-	value={"reqType":0, "perception":{"inputText":{"text":msg.text}}, "userInfo":{"apiKey":"501dba59c7f54555b0d9b66f4acabd94","userId":"235827"}}
-	req=requests.post(url, data=json.dumps(value))
-	a=eval(req.text)
-	reply = a.get("results")[0].get('values').get('text')
-	print("{\n\'msg\':\t\t%s\n\'reply\':\t%s\n}\n" % (msg.text, reply))
-	return reply
-itchat.auto_login(enableCmdQR=2, hotReload=True)
-itchat.run()
+    if msg_check_text_before_reply(msg) == 'True':
+        return "auto reply is enabled"
+    if msg_check_text_before_reply(msg) == 'False':
+        return "auto reply is disabled"
+    if not mIsItchatEnabled:
+        return
+    value = {"reqType": 0, "perception": {"inputText": {"text": msg.text}},
+             "userInfo": {"apiKey": "501dba59c7f54555b0d9b66f4acabd94", "userId": "235827"}}
+    req = requests.post(url, data=json.dumps(value))
+    a = eval(req.text)
+    reply = a.get("results")[0].get('values').get('text')
+    print("{\n\'msg\':\t\t%s\n\'reply\':\t%s\n}\n" % (msg.text, reply))
+    return reply
+
+
+def image_reply(msg):
+    pass
+
+
+def msg_check_text_before_reply(msg):
+    global mIsItchatEnabled
+    if str.lower(msg.text) == 'true':
+        mIsItchatEnabled = True
+        return 'True'
+    if str.lower(msg.text) == 'false':
+        mIsItchatEnabled = False
+        return 'False'
+    return
+
+
+if __name__ == '__main__':
+    itchat.auto_login(enableCmdQR=2, hotReload=True)
+    itchat.run()
